21fc9a39ce4d3cce241b525fa728b3af524211b5
root <root@HO2018100003.ichiho.net>
2022-06-17 14:40:57 +0900

    tei

---
+++ ckeditor5/src/ckeditor.js
@@ -0,0 +1,123 @@
+/**
+ * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave.js';
+import Base64UploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import CloudServices from '@ckeditor/ckeditor5-cloud-services/src/cloudservices.js';
+import DataFilter from '@ckeditor/ckeditor5-html-support/src/datafilter.js';
+import DataSchema from '@ckeditor/ckeditor5-html-support/src/dataschema.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import ExportPdf from '@ckeditor/ckeditor5-export-pdf/src/exportpdf.js';
+import ExportWord from '@ckeditor/ckeditor5-export-word/src/exportword.js';
+import FindAndReplace from '@ckeditor/ckeditor5-find-and-replace/src/findandreplace.js';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import FontFamily from '@ckeditor/ckeditor5-font/src/fontfamily.js';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight.js';
+import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import EditorWatchdog from '@ckeditor/ckeditor5-watchdog/src/editorwatchdog.js';
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+	Alignment,
+	Autosave,
+	Base64UploadAdapter,
+	Bold,
+	CloudServices,
+	DataFilter,
+	DataSchema,
+	Essentials,
+	ExportPdf,
+	ExportWord,
+	FindAndReplace,
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+	GeneralHtmlSupport,
+	Heading,
+	Highlight,
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	Indent,
+	Italic,
+	Link,
+	List,
+	PageBreak,
+	Paragraph,
+	PasteFromOffice,
+	SourceEditing,
+	Table,
+	TableToolbar,
+	TextTransformation
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+	toolbar: {
+		items: [
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'|',
+			'outdent',
+			'indent',
+			'|',
+			'imageUpload',
+			'insertTable',
+			'undo',
+			'redo',
+			'|',
+			'sourceEditing'
+		]
+	},
+	language: 'ja',
+	image: {
+		toolbar: [
+			'imageTextAlternative',
+			'imageStyle:inline',
+			'imageStyle:block',
+			'imageStyle:side'
+		]
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn',
+			'tableRow',
+			'mergeTableCells'
+		]
+	}
+};
+
+export default { Editor, EditorWatchdog };
--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {  useState } from 'react';
+import {Editor as ClassicEditor} from 'ckeditor5-custom-build/build/ckeditor';
+import { CKEditor  } from '@ckeditor/ckeditor5-react';
+import './styles/content-styles-editor.css';
+//import './styles/content-styles-front.css';
+import './styles/contentsdb_style.css';
+import xhtml from './mock/test1/xhtml'
+import { Col, Row, Space, Button } from 'antd';
+import parse from 'html-react-parser'
+import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const editorConfiguration = {
+    // toolbar: [ 'bold', 'italic' ]
+    heading: {
+        options: [
+            { model: 'heading1', view:'h1', title: '見出し：タイトル' },
+            { model: 'paragraph', view: 'p', title: '見出し：Q', class: 'question' },
+            { model: 'paragraph', view: 'p', title: '見出し：A', class: 'answer' }
+        ]
+    },
+    htmlSupport: {
+        allow: [
+            {
+                name: /.*/,
+                attributes: true,
+                classes: true,
+                styles: true
+            }
+        ]
+    }
+};
+
+const App = () => {
+
+    const [data,setData] = useState(xhtml.data);
+    const [model,setModel] = useState(1);
+
+    const editorChangeHandle = (data)=>{
+        setData(data);
+    }
+
+    return (
+        <>
+        <Row>
+            <Col span={12}>
+                <div>
+                <CKEditor
+                    editor={ ClassicEditor }
+                    config={ editorConfiguration }
+                    data={data}
+                    onReady={ editor => {
+                        CKEditorInspector.attach( editor );
+                        this.editor = editor;
+                    } }
+                    onChange={ ( event, editor ) => {
+                        const data = editor.getData();
+                        editorChangeHandle(data);
+                    } }
+                    onBlur={ ( event, editor ) => {
+                        console.log( 'Blur.', editor );
+                    } }
+                    onFocus={ ( event, editor ) => {
+                        console.log( 'Focus.', editor );
+                    } }
+                />
+                </div>
+            </Col>
+            <Col span={12}>
+                <Space>
+                    <Button onClick={()=>{setModel(1)}}>source model</Button>
+                    <Button onClick={()=>{setModel(2)}}>html model</Button>
+                </Space>
+                <br/>
+                <br/>
+                {model ===1?(<div>{data}</div>):null}
+                {model ===2?(<div>{parse(data)}</div>):null}
+            </Col>
+        </Row>
+        </>
+    );
 }
 
 export default App;
